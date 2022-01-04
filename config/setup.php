b7efb2b804c6ef757314bace71f7e8977e3c046f
Estevão Rolim <estevao.prolim@gmail.com>
2022-01-04 17:39:20 +0000

    Add to allow GET, POST, PUT and DELETE methods

--- config/setup.php
+++ config/setup.php
@@ -3,6 +3,7 @@
     // Headers
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Origin, Content-Type, Access-Control-Allow-Methods, Access-Control-Allow-Headers, Authorization, X-Requested-With, Authentication');
 
     require __DIR__ . '/../vendor/autoload.php';
